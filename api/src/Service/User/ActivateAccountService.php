436ac17c2df3383613fd40d4190ef938b5d95c52
Carlos Licto <carlosdarkslayer@gmail.com>
2023-05-30 23:44:11 -0500

    service para activar cuenta del usuario

---
+++ api/src/Service/User/ActivateAccountService.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\User;
+
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\Exception\ORMException;
+
+class ActivateAccountService
+{
+    /**
+     * @var UserRepository
+     */
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function activate(string $id, string $token): User
+    {
+        $user = $this->userRepository->findOneInactiveByIdAndTokenOrFail($id, $token);
+
+        $user->setActive(true);
+        $user->setToken(null);
+
+        $this->userRepository->save($user);
+
+        return $user;
+    }
+}

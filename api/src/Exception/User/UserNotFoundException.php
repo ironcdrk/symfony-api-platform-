e1eed07e22b9aa1045df3093ab40bd940dafd18c
Carlos Licto <carlosdarkslayer@gmail.com>
2023-06-06 22:43:26 -0500

    creación mensaje de excepcón para busqueda por reset password token

--- api/src/Exception/User/UserNotFoundException.php
+++ api/src/Exception/User/UserNotFoundException.php
@@ -20,4 +20,8 @@ class UserNotFoundException extends NotFoundHttpException
         throw new self(\sprintf('User with id: %s and token: %s not found', $id,$token));
     }
 
+    public static function fromUserIdAndResetPasswordToken(string $id, string $resetPasswordToken): self
+    {
+        throw new self(\sprintf('User with id %s and resetPasswordToken %s not found', $id, $resetPasswordToken));
+    }
 }

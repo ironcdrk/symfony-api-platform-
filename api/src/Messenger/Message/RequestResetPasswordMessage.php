ddb82509d59e1c29c74c56ac2cad119fc27b35a3
Carlos Licto <carlosdarkslayer@gmail.com>
2023-06-06 21:52:22 -0500

    clase para "mapear" mensaje de reseteo de password

---
+++ api/src/Messenger/Message/RequestResetPasswordMessage.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Messenger\Message;
+
+
+class RequestResetPasswordMessage
+{
+    /**
+     * @var String
+     */
+    private String $id;
+    /**
+     * @var string
+     */
+    private string $email;
+    /**
+     * @var string
+     */
+    private string $resetPasswordToken;
+
+    public function __construct(String $id, string $email, string $resetPasswordToken)
+    {
+        $this->id = $id;
+        $this->email = $email;
+        $this->resetPasswordToken = $resetPasswordToken;
+    }
+
+    /**
+     * @return String
+     */
+    public function getId(): String
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getResetPasswordToken(): string
+    {
+        return $this->resetPasswordToken;
+    }
+
+
+
+}
